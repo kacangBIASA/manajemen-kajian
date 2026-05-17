6522d4093b2f61e9e1d26fdf9affd6c2176cd874
kacangBIASA <kacangBIASA>
2026-05-17 18:57:09 +0700

    fix vite manifest not found

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,6 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    
     public function run(): void
     {
         $this->call([AdminUserSeeder::class]);
